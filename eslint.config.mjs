92fd0fd80ec33e6a536b989b622ae18067dc1cf3
PawarisSueaaeim <pawaris.sueaaeim@gmail.com>
2025-03-16 23:29:42 +0700

    fix: config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,44 +1,39 @@
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 import { FlatCompat } from "@eslint/eslintrc";
-import tsParser from "@typescript-eslint/parser";
-import js from "@eslint/js";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const compat = new FlatCompat({
-    baseDirectory: __dirname,
+  baseDirectory: __dirname,
 });
 
 const eslintConfig = [
-    js.configs.recommended,
-    {
-        plugins: ["prettier", "@typescript-eslint"],
-        files: ["**/*.ts", "**/*.tsx"],
-        languageOptions: {
-            parser: tsParser,
-            parserOptions: {
-                sourceType: "module",
-                ecmaVersion: "latest",
-            },
-        },
-        rules: {
-            "prettier/prettier": "error",
-            
-            // ปิด warning ของ react-hooks/exhaustive-deps
-            "react-hooks/exhaustive-deps": "off",
-
-            // อนุญาตให้ใช้ {} เป็น type
-            "@typescript-eslint/no-empty-object-type": "off",
-
-            // อนุญาตให้ใช้ any
-            "@typescript-eslint/no-explicit-any": "off",
-
-            // ปิด error ที่เกิดจากตัวแปรที่ไม่ได้ใช้ (เปลี่ยนเป็น warning)
-            "@typescript-eslint/no-unused-vars": "warn",
-        },
+  // ...compat.extends("next/core-web-vitals", "next/typescript", "prettier", "eslint:recommended", "next" ),
+  js.configs.recommended,
+  {
+    plugins: ["prettier"],
+    files: ["**/*.ts", "**/*.tsx"],
+    languageOptions: {
+      parser: tsParser,
     },
+    rules: {
+      "prettier/prettier": "error",
+
+      // ปิด warning ของ react-hooks/exhaustive-deps
+      "react-hooks/exhaustive-deps": "off",
+
+      // อนุญาตให้ใช้ {} เป็น type
+      "@typescript-eslint/no-empty-object-type": "off",
+
+      // อนุญาตให้ใช้ any
+      "@typescript-eslint/no-explicit-any": "off",
+
+      // ปิด error ที่เกิดจากตัวแปรที่ไม่ได้ใช้
+      "@typescript-eslint/no-unused-vars": "warn",
+    }
+  },
 ];
 
 export default eslintConfig;
